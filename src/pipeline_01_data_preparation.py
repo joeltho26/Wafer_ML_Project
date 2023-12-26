268f423b5ce00b4fbe0ff40cc790d27630b059d0
jl682450 <66740724+jl682450@users.noreply.github.com>
2023-12-26 14:25:35 -0500

    data prep file modified

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -3,11 +3,21 @@ import argparse
 import yaml
 import logging
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def main(config_path, datasource):
+    config = read_params(config_path)
+    print(config)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config",default="default")
+    default_config = os.path.join("config","params.yaml")
+    parser.add_argument("--config",default=default_config)
     parser.add_argument("--datasource",default=None)
     parsed_args = parser.parse_args()
     print(parsed_args.config,parsed_args.datasource)
+    main(parsed_args.config,parsed_args.datasource)
