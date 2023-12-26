d123ec8b0cb3cac80fde48f845fb4ea3648ef1ed
jl682450 <66740724+jl682450@users.noreply.github.com>
2023-12-26 12:38:54 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its a MLOPS project for wafer',
+    author='joeltho',
+    license='MIT',
+)
